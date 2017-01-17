cb294ac6cde0de77cce9b9256dc5bfb251526c72
guai3 <takekuma@gmail.com>
2017-01-17 16:44:53 +0900

    テーブルレイアウトの作成

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -13,16 +13,18 @@
       <th data-type ="string">送り状番号</th>
       <th data-type ="string">メモ</th>
       <th data-type ="string">荷物状況</th>
-      <th data-type ="string">公式サイト</th>
-      <th data-type ="string">編集</t>
+      <th data-type ="string">編集</th>
     </tr>
   </thead>
   <tbody>
   @forelse ($invoices as $invoice)
   <tr>
-    <td><a href="">{{ $invoice->invoice_number }}</a></td>
-    <td><a href ="/invoices/{{ $invoice->company}}">{{ $invoice->company }}</a></td>
-    <td><a href ="{{url('/invoices/', $invoice->id)}}">{{ $invoice->company }}</a></td>
+    <td><a href="">{{ $invoice->shipping_date }}</a></td>
+    <td><a href="">{{ $invoice->company }}</a></td>
+    <td><a href="http://google.com">{{ $invoice->invoice_number }}</a></td>
+    <td><a href="">{{ $invoice->memo }}</a></td>
+    <td><a href="">{{ $invoice->status }}</a></td>
+    <td><a href="">{{ $invoice->company }}</a></td>
   </tr>
   @empty
   <tr><td>No data</td><td>No data</td><td>No data</td> </tr>
