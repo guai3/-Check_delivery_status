92c1c644803ef183ca3604c45b3c58aeca4b6180
guai3 <takekuma@gmail.com>
2017-01-17 16:30:14 +0900

    cssの修正

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -4,26 +4,23 @@
 
 @section('content')
 <h1>配達状況確認ツール</h1>
-<!-- <ul>
-  @foreach ($invoices as $invoice)
-  <li><a href="">{{ $invoice->invoice_number }}</a></li>
-  @endforeach
-
-</ul> -->
 
 <table>
   <thead>
     <tr>
-      <th data-type ="string">invoce</th>
-      <th data-type ="string">Team</th>
-      <th data-type ="number">Score</t>
+      <th data-type ="string">発送日</th>
+      <th data-type ="string">運送会社</th>
+      <th data-type ="string">送り状番号</th>
+      <th data-type ="string">メモ</th>
+      <th data-type ="string">荷物状況</th>
+      <th data-type ="string">公式サイト</th>
+      <th data-type ="string">編集</t>
     </tr>
   </thead>
   <tbody>
   @forelse ($invoices as $invoice)
   <tr>
     <td><a href="">{{ $invoice->invoice_number }}</a></td>
-
     <td><a href ="/invoices/{{ $invoice->company}}">{{ $invoice->company }}</a></td>
     <td><a href ="{{url('/invoices/', $invoice->id)}}">{{ $invoice->company }}</a></td>
   </tr>
